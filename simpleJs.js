3ae82961243fdad3e18cdd24224914b187e89781
Chingis007 <karaulnekerell@gmail.com>
2022-10-18 21:54:59 +0300

    мпрнг

--- simpleJs.js
+++ simpleJs.js
@@ -1,6 +1,6 @@
 function permission(){
     var value = document.getElementById("paswordInput").value;
-    if (value == "1111"){//1910
+    if (value == "1910"){
     document.getElementById("disBut").removeAttribute("disabled")
     }else{
         window.alert("Wrong!")
